b56ecdfea42c0dbc6cc2dac04b4bbd6081373890
Sune <fengel@gmail.com>
2018-06-01 12:50:16 +0200

    linear fetch

--- src/modules/organisms/Home/index.js
+++ src/modules/organisms/Home/index.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react'
 
 class Home extends Component {
   render() {
-    const pages = this.props.pages
-    const posts = this.props.posts
-    
+    const {pages, posts, menus} = this.props
+    console.dir(pages)
+    console.dir(posts)
+    console.dir(menus)
     return (
       <div className="org-a9ca065c-a467-47bb-a252-2138bff1cb1d">
         <h1>Home</h1>
--- src/wpService.js
+++ src/wpService.js
@@ -1,46 +1,55 @@
-
 import request from 'request-promise'
 
 const routes = {
     uri: 'http://fengel.com/wp-json',
     pages: '/wp/v2/pages/',
-    posts: '/wp/v2/posts/'
+    posts: '/wp/v2/posts/',
+    menus: '/menus/v1/menus',
 }
 
 const fetchRoot = () => {
-    return request(
-        {
+    return request({
             uri: routes.uri,
-        }
-    )
-    .then(response => JSON.parse(response))
-    .catch(err => {
-        console.log('Err', err)
-    })
+        })
+        .then(response => JSON.parse(response))
+        .catch(err => {
+            console.log('Err', err)
+        })
 }
 
 const fetchPages = () => {
-    return request(
-        {
+    return request({
             uri: routes.uri + routes.pages,
-        }
-    )
-    .then(response => JSON.parse(response))
-    .catch(err => {
-        console.log('Err', err)
-    })
+        })
+        .then(response => JSON.parse(response))
+        .catch(err => {
+            console.log('Err', err)
+        })
 }
 
 const fetchPosts = () => {
-    return request(
-        {
+    return request({
             uri: routes.uri + routes.posts,
-        }
-    )
-    .then(response => JSON.parse(response))
-    .catch(err => {
-        console.log('Err', err)
-    })
+        })
+        .then(response => JSON.parse(response))
+        .catch(err => {
+            console.log('Err', err)
+        })
 }
 
-export { fetchRoot, fetchPages, fetchPosts }
+const fetchMenus = () => {
+    return request({
+            uri: routes.uri + routes.posts,
+        })
+        .then(response => JSON.parse(response))
+        .catch(err => {
+            console.log('Err', err)
+        })
+}
+
+export {
+    fetchRoot,
+    fetchPages,
+    fetchPosts,
+    fetchMenus
+}
