28bc0585facf8380230b391ca6b9b6511f28cbee
Sune <fengel@gmail.com>
2018-06-04 20:08:15 +0200

    Added hme btn

--- src/modules/molecules/Aside/index.js
+++ src/modules/molecules/Aside/index.js
@@ -1,6 +1,7 @@
-import React, { PureComponent } from 'react'
+import React, { PureComponent } from 'react';
+import { Link } from 'react-router-dom';
+import './styles.scss';
 
-import './styles.scss'
 
 class Aside extends PureComponent {
   render() {
@@ -8,10 +9,11 @@ class Aside extends PureComponent {
       <aside className="mol-2a2c1466-55b6-4b02-9bd9-3261469c7c29">
         <nav>
           <ul>
+            <li><Link to={`/`}>Home</Link></li>
             {this.props.categories.map(cat => {
               return (
                 <li key={cat.id}>
-                  <a href={`/${cat.taxonomy}/${cat.slug}`}>{cat.name}</a>
+                  <Link to={`/${cat.taxonomy}/${cat.slug}`}>{cat.name}</Link>
                 </li>
               )
             })}
