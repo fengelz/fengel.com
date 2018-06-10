0bf6e454f91e5cd11b60ba22dedb6f76b3b40ca1
Sune <fengel@gmail.com>
2018-06-10 20:16:13 +0200

    Minor seo update attempts

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+var BabelPlugin = require('babel-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -50,4 +51,25 @@ module.exports = {
     port: 9000,
     historyApiFallback: true,
   },
+  plugins: [
+    new BabelPlugin({
+      test: /\.js$/,
+      presets: [
+        [
+          'env',
+          {
+            exclude: ['transform-regenerator'],
+            loose: true,
+            modules: false,
+            targets: {
+              browsers: ['>1%'],
+            },
+            useBuiltIns: true,
+          },
+        ],
+      ],
+      sourceMaps: false,
+      compact: false,
+    }),
+  ],
 }
